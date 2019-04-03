b778c687315ba3f9372483a5727fc99c3c9f705f
Aftab Shaikh <44019928+affishaikh@users.noreply.github.com>
2019-04-03 17:52:35 +0530

    Updates

---
+++ precache-manifest.f2380d6fbed124a643034d282e05fe9c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5a3fc8d14b65c7970df8",
+    "url": "/game_of_life/static/js/runtime~main.7abc2d87.js"
+  },
+  {
+    "revision": "3d1d7983caea11e1f7ff",
+    "url": "/game_of_life/static/js/main.0e95ce9d.chunk.js"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/game_of_life/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "3d1d7983caea11e1f7ff",
+    "url": "/game_of_life/static/css/main.8a1b869c.chunk.css"
+  },
+  {
+    "revision": "39f3fa3a04f8b3b0039f8e884a1cebbd",
+    "url": "/game_of_life/index.html"
+  }
+];
