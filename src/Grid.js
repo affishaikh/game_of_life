1eb9002704f1e4bfa5990bb6b3eb4dfcbd1d52ae
Aftab Shaikh <44019928+affishaikh@users.noreply.github.com>
2019-04-03 12:10:48 +0530

    Create a grid for UI
    The game will continue

---
+++ src/Grid.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+
+const Data = function(props) {
+  const { element } = props;
+  return <td>{element}</td>;
+};
+
+const Row = function(props) {
+  const {row} = props;
+  return (
+    <tr>
+      {row.map(element => (
+        <Data element={element} />
+      ))}
+    </tr>
+  );
+};
+
+const Body = function(props) {
+  const {grid} = props;
+  return (
+    <tbody>
+      {grid.map(row => (
+        <Row row={row} />
+      ))}
+    </tbody>
+  );
+};
+
+class Table extends Component {
+  render() {
+    return (
+      <table className="grid" id="grid">
+        <Body grid={this.props.grid} />
+      </table>
+    );
+  }
+}
+
+export default Table;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,55 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import Grid from './Grid.js';
+import { nextGeneration } from './models/gameOfLife';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const emptyGrid = [
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 1, 1, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+];
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const getCopyOf = grid => grid.map(row => row.slice());
+
+class World extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      grid: getCopyOf(emptyGrid)
+    };
+  }
+
+  play() {
+    const aliveCells = nextGeneration([[4, 4], [4, 5], [5, 5]], {
+      topLeft: [0, 0],
+      bottomRight: [9, 9]
+    });
+
+    console.log(aliveCells);
+
+    this.setState(() => {
+      const grid = getCopyOf(emptyGrid);
+      aliveCells.forEach(cell => {
+        grid[cell[0]][cell[1]] = 1;
+      });
+      return { grid };
+    });
+  }
+
+  componentDidMount() {
+    setInterval(this.play.bind(this), 1000);
+  }
+
+  render() {
+    return <Grid grid={this.state.grid} />;
+  }
+}
+
+ReactDOM.render(<World />, document.getElementById('root'));
